8251690d6259b41fe00ff85b98f388cac8a78ebf
Joaquincuevas <jjcuevas@miuandes.cl>
2025-04-30 16:44:32 -0400

    Lab07

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,2 +1,17 @@
 class Chat < ApplicationRecord
+    has_many :messages
+    belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
+    belongs_to :receiver, class_name: 'User', foreign_key: 'receiver_id'
+
+    validates :sender_id, presence: true
+    validates :receiver_id, presence: true
+    validate :sender_different_receiver
+
+    private
+
+    def sender_different_receiver
+        if sender_id == receiver_id
+            errors.add(:receiver_id, "must be different from sender")
+        end
+    end
 end
--- db/migrate/20250409205213_addtable_message.rb
+++ db/migrate/20250409205213_addtable_message.rb
@@ -1,8 +1,8 @@
 class AddtableMessage < ActiveRecord::Migration[8.0]
   def change
     create_table :messages do |t|
-      t.integer :chat_id, null: false
-      t.integer :user_id, null: false
+      t.references :chat, null: false
+      t.references :user, null: false
       t.text :body, null: false
       t.timestamps
     end
