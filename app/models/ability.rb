ae25657816fd11e5c3c8d65421a256efd268fa74
Joaquincuevas <jjcuevas@miuandes.cl>
2025-05-28 17:17:54 -0400

    Lab 09 - Progress 1.1

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,5 +7,9 @@ class Ability
     can :read, :all
     can :create, Message
     can [:update, :destroy], Message, user_id: user.id
+    can [:edit, :update, :destroy], User, id: user.id
+    can [:edit, :update, :destroy], Chat do |chat|
+      chat.sender_id == user.id || chat.receiver_id == user.id
+    end
   end
 end
